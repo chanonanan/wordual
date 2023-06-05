ad47fc41ad87e8039dbf53b0fcb66a3aa6352cb9
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2023-06-05 16:17:19 +0700

    fix typo

--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { CreateGameEventHandlerService } from '@services/event/handler/create-game.service';
 import { FindGameEventHandlerService } from '@services/event/handler/find-game.service';
-import { JoinGameEventHandlerService } from '@services/event/handler/join-game,service';
+import { JoinGameEventHandlerService } from '@services/event/handler/join-game.service';
 
 @Injectable({
   providedIn: 'root'
