10f9d9dc0f163e3537df4c83358948dfb7370cc2
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2023-06-06 11:17:20 +0700

    update toast style

--- src/app/services/toast/toast.service.ts
+++ src/app/services/toast/toast.service.ts
@@ -1,63 +1,28 @@
 import { Injectable } from '@angular/core';
-import { timer } from 'rxjs';
-import { first, take } from 'rxjs/operators';
+import { Observable, delay, first, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
-  private toastElement: HTMLDivElement | null = null;
-  private toastQueue: { message: string, type: string }[] = [];
-  private isToastShowing = false;
-
   showToast(message: string, type: 'error' | 'success'): void {
-    const toast = { message, type };
-    this.toastQueue.push(toast);
-
-    if (!this.isToastShowing) {
-      this.showNextToast();
-    }
-  }
-
-  private showNextToast(): void {
-    if (this.toastQueue.length === 0) {
-      return;
-    }
-
-    const toast = this.toastQueue.shift()!;
-    this.createToastElement(toast.message, toast.type);
-
-    timer(100).pipe(first()).subscribe(() => {
-      this.showToastElement();
-
-      timer(3000).pipe(take(1)).subscribe(() => {
-        this.hideToastElement();
-        timer(600).pipe(take(1)).subscribe(() => {
-          document.body.removeChild(this.toastElement!);
-          this.toastElement = null;
-          this.showNextToast();
-        });
-      });
-    });
+    this.createToast(message, type).pipe(
+      tap(toast => document.body.appendChild(toast)),
+      delay(100),
+      tap(toast => toast.classList.add('show')),
+      delay(3000),
+      tap(toast => toast.classList.remove('show')),
+      delay(1000), // wait for animation done
+      tap(toast => document.body.removeChild(toast)),
+      first(),
+    ).subscribe();
 
   }
 
-  private createToastElement(message: string, type: string): void {
-    if (!this.toastElement) {
-      this.toastElement = document.createElement('div');
-      document.body.appendChild(this.toastElement);
-    }
-    this.toastElement.innerText = message;
-    this.toastElement.className = `toast-message ${type}`;
-  }
-
-  private showToastElement(): void {
-    this.toastElement!.classList.add('show');
-    this.isToastShowing = true;
-  }
-
-  private hideToastElement(): void {
-    this.toastElement!.classList.remove('show');
-    this.isToastShowing = false;
+  private createToast(message: string, type: 'error' | 'success'): Observable<HTMLDivElement> {
+    const toastElement = document.createElement('div');
+    toastElement.className = `toast-message ${type}`;
+    toastElement.innerText = message;
+    return of(toastElement);
   }
 }
